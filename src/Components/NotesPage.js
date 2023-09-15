35c85fa9e3b36d0a3c991524fbf58dd1b673518b
Kireet <singhkireet5@gmail.com>
2023-09-15 16:06:22 +0530

    added delete functionality

--- src/Components/NotesPage.js
+++ src/Components/NotesPage.js
@@ -1,13 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getTodoItems } from "../features/todo/todoSlice";
+import {
+  getTodoItems,
+  postTodoItems,
+  deleteTodoItems,
+} from "../features/todo/todoSlice";
 const NotesPage = () => {
   const { todoItems, isLoading } = useSelector((store) => store.todo);
   const dispatch = useDispatch();
+  const [pressed, setPressed] = useState(false);
   useEffect(() => {
     dispatch(getTodoItems());
     // eslint-disable-next-line
-  }, []);
+  }, [pressed]);
+  const [name, setName] = useState("");
+  const date = "12/1/23";
+  const tag = "personal";
   if (isLoading) {
     return (
       <>
@@ -23,25 +31,57 @@ const NotesPage = () => {
             name="notesinput"
             className="bg-slate-700 text-white"
             id=""
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
             cols="40"
             rows="15"
           ></textarea>
         </div>
         <div>
-          <button className="bg-blue-500 flex items-center justify-center h-10 rounded-lg p-4 mx-2 my-4 text-white font-semibold">
+          <button
+            className="bg-blue-500 flex items-center justify-center h-10 rounded-lg p-4 mx-2 my-4 text-white font-semibold"
+            onClick={() => {
+              dispatch(postTodoItems({ name, date, tag }));
+              setPressed(true);
+              setTimeout(() => {
+                setPressed(false);
+              }, 50);
+            }}
+          >
             Add Note
           </button>
         </div>
 
-        <div className="flex flex-wrap justify-center items-center ">
+        <div className="flex flex-col justify-center items-center ">
           {todoItems.map((item) => (
             <div
               key={item._id}
-              className="bg-slate-800 rounded-lg h-28 mx-3 w-72 flex flex-col items-start justify-around my-6 p-7"
+              className=" bg-slate-800 rounded-lg h-28 mx-3 w-[70vw]  my-6 p-2 sm:p-4 md:p-7 justify-between flex  items-center"
             >
-              <div className="text-lg text-white font-bold">{item.name}</div>
-              <div className="text-base text-white  italic">
-                {item.tag} {item.date}
+              <div className="flex flex-col items-start justify-around">
+                <div className="text-lg text-white font-bold">{item.name}</div>
+                <div className="text-base text-white  italic">
+                  {item.tag} {item.date}
+                </div>
+              </div>
+              <div className="flex flex-col sm:flex-row box-border">
+                <button
+                  onClick={() => {
+                    dispatch(deleteTodoItems(item._id));
+                    setPressed(true);
+                    setTimeout(() => {
+                      setPressed(false);
+                    }, 50);
+                  }}
+                  className="bg-red-500 p-1 h-8 sm:p-2 sm:h-14 rounded-lg text-white font-semibold mx-3 my-3"
+                >
+                  Delete
+                </button>
+                <button className="bg-blue-500 p-1 h-8 sm:p-2 sm:h-14 rounded-lg text-white font-semibold mx-3 my-3">
+                  Update
+                </button>
               </div>
             </div>
           ))}
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -22,6 +22,53 @@ export const getTodoItems = createAsyncThunk("todo/gettodoitems", async () => {
     throw error;
   }
 });
+export const postTodoItems = createAsyncThunk(
+  "todo/posttodoitems",
+  async (newdata) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/api/todo/add",
+        newdata,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token":
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjUwMmEyMmRkNjdkNzYwZTVkMzM2YWFjIn0sImlhdCI6MTY5NDY3MTQwNX0.OCZv2vL6xRe0xaEcO1ZMZ25ZnVHjFbTAnSxMpS00d2I",
+          },
+        }
+      );
+
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+);
+
+export const deleteTodoItems = createAsyncThunk(
+  "todo/deletetodoitems",
+  async (id) => {
+    try {
+      const response = await axios.delete(
+        `http://localhost:4000/api/todo/delete/${id}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token":
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjUwMmEyMmRkNjdkNzYwZTVkMzM2YWFjIn0sImlhdCI6MTY5NDY3MTQwNX0.OCZv2vL6xRe0xaEcO1ZMZ25ZnVHjFbTAnSxMpS00d2I",
+          },
+        }
+      );
+
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+);
+
 const todoSlice = createSlice({
   name: "todo",
   initialState,
@@ -35,6 +82,26 @@ const todoSlice = createSlice({
       console.log(action);
     },
     [getTodoItems.rejected]: (state) => {
+      state.isLoading = false;
+    },
+
+    [postTodoItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [postTodoItems.fulfilled]: (state, action) => {
+      state.isLoading = false;
+    },
+    [postTodoItems.rejected]: (state) => {
+      state.isLoading = false;
+    },
+
+    [deleteTodoItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [deleteTodoItems.fulfilled]: (state, action) => {
+      state.isLoading = false;
+    },
+    [deleteTodoItems.rejected]: (state) => {
       state.isLoading = false;
     },
   },
