93e27034b10942f363f7244187c0c3070e60f8ed
Kireet <singhkireet5@gmail.com>
2023-09-20 18:39:00 +0530

    loginSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./../features/todo/todoSlice";
 import signupReducer from "../features/signup/signupSlice";
+import loginReducer from "../features/login/loginSlice";
 const store = configureStore({
   reducer: {
     todo: todoReducer,
     signup: signupReducer,
+    login: loginReducer,
   },
 });
 
--- src/features/login/loginSlice.js
+++ src/features/login/loginSlice.js
@@ -1,11 +1,54 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
 const initialState = {
+  isLoading: true,
+  error: false,
   token: "",
-  isLoggedin: false,
 };
+
+export const login = createAsyncThunk("login/", async (userdata) => {
+  try {
+    const resp = await axios.post(
+      "http://localhost:4000/api/auth/login",
+      userdata,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return resp.data;
+  } catch (err) {
+    console.error("error logging in ", err);
+    throw err;
+  }
+});
 const loginSlice = createSlice({
   name: "login",
   initialState,
+  reducers: {
+    addToken: (state, action) => {
+      state.token = action.payload;
+    },
+  },
+  extraReducers: {
+    [login.pending]: (state) => {
+      state.isLoading = true;
+      state.error = false;
+    },
+    [login.fulfilled]: (state) => {
+      console.log("fulfilled");
+      state.isLoading = false;
+      state.error = false;
+    },
+    [login.rejected]: (state) => {
+      console.log("rejected");
+      state.isLoading = false;
+      state.error = true;
+    },
+  },
 });
-
-export default loginSlice;
+export const { addToken } = loginSlice.actions;
+export default loginSlice.reducer;
